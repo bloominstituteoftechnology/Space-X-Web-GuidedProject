7f1a1abfe38730fead610d233d28df83fc5ffcc5
Dustin Myers <dustin@lambdaschool.com>
2020-03-10 14:48:48 -0600

    set up start repo

--- src/components/MissionsList.js
+++ src/components/MissionsList.js
@@ -1,21 +1,14 @@
 import React from "react";
 
 const MissionsList = props => {
-  console.log(props.missions);
   return (
     <section className="missions-list">
       {props.error ? (
-        <div className="error" data-testid="missions-list-error">
-          {props.error}
-        </div>
+        <div className="error">{props.error}</div>
       ) : (
-        <div data-testid="mission-list">
+        <div>
           {props.missions.map(mission => (
-            <div
-              className="mission"
-              data-testid="mission"
-              key={mission.mission_id}
-            >
+            <div className="mission" key={mission.mission_id}>
               {mission.mission_name}
             </div>
           ))}
